bbcbd01246a47a839057c32b57c37ef86d4de7a9
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-04-03 10:57:42 +0300

    Add files via upload

--- бинарный.py
+++ бинарный.py
@@ -1,9 +1,11 @@
 import random
 
 a = random.randint(1, 100)
+i = 0
 
 while True:
     b = int(input('введите число: '))
+    i += 1
     if b > a:
         print('загаданное число меньше')
         continue
@@ -13,3 +15,4 @@ while True:
     else:
         print('ВЫ ОТГАДАЛИ ЧИСЛО')
     break
+print('количество попыток: ', i)
